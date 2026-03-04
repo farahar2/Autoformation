c01495e6373ec8f87c0c9cc7575088a979ff723c
farahar2 <farahafouzar@gmail.com>
2026-03-04 14:56:01 +0100

    first commit

---
+++ Challenge.php
@@ -0,0 +1,54 @@
+<?php
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $prix_the = $_POST['prix_the'];
+    $nombre_the = $_POST['nombre_the'];
+    $Etudiant = isset($_POST['etudiant']);
+
+    $prix_brut = $prix_the * $nombre_the;
+
+    if($Etudiant == true){
+        $prix_total = $prix_brut * 0.2;
+    }
+    elseif($nombre_the > 5){
+        $prix_total = $prix_brut - $nombre_the;
+    }
+    else{
+        $prix_total = $prix_brut;
+    }
+
+    echo "Le prix total de ta commande est : " . $prix_total . " DH";
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Commande de thé</title>
+</head>
+<body>
+
+<h2>Calculer le prix de votre commande de thé</h2>
+
+<form method="POST">
+
+Prix d'un thé :
+<input type="number" name="prix_the" required>
+<br><br>
+
+Nombre de thés :
+<input type="number" name="nombre_the" required>
+<br><br>
+
+Êtes-vous étudiant ?
+<input type="checkbox" name="etudiant">
+<br><br>
+
+<button type="submit">Calculer</button>
+
+</form>
+
+</body>
+</html>
