4cb0b184197d4a7afca8f27f0d4c2678e33018a1
farahar2 <farahafouzar@gmail.com>
2026-03-05 13:41:49 +0100

    new update

--- Challenge3.php
+++ Challenge3.php
@@ -6,6 +6,26 @@
   <title>Document</title>
 </head>
 <body>
-  
+  <?php
+$friends = [
+  "Mehdi" => 200,
+  "Farah" => 35,
+  "Raouya" => 110,
+  "Najiba" => 60,
+  "Samah" => 98
+];
+
+$sum = 0;
+
+foreach($friends as $key => $value){
+    $sum += $value;
+
+    if($value > 100){
+        echo $key . " owes more than 100 DH<br>";
+    }
+}
+
+echo "The total is " . $sum;
+?>
 </body>
 </html>
